162015daddb93fe7121310562f6562c64047bf41
Saharat Paynok <saharat.paynok@gmail.com>
2019-09-08 18:12:17 +0700

    [Lec 87]: Working with ListViews

--- lib/widgets/transactoin_list.dart
+++ lib/widgets/transactoin_list.dart
@@ -11,57 +11,72 @@ class TransactonList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 250,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tx) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 15,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 2,
-                      ),
-                    ),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      '\$${tx.amount}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
-                      ),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        DateFormat.yMMMd().format(tx.date),
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+      child: buildListViewWithBuilder(),// Or use buildListViewWithChildren
+    );
+  }
+
+  ListView buildListViewWithBuilder() {
+    return ListView.builder(
+      itemBuilder: (buildcontext, index) {
+        return buildTransactionCard(transactions[index]);
+      },
+      itemCount: transactions.length,
+    );
+  }
+
+  ListView buildListViewWithChildren() {
+    return ListView(
+      children: transactions.map((tx) {
+        return buildTransactionCard(tx);
+      }).toList(),
+    );
+  }
+
+  Card buildTransactionCard(Transaction tx) {
+    return Card(
+      child: Row(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.symmetric(
+              vertical: 10,
+              horizontal: 15,
+            ),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+                width: 2,
               ),
-            );
-          }).toList(),
-        ),
+            ),
+            padding: EdgeInsets.all(10),
+            child: Text(
+              '\$${tx.amount}',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Colors.purple,
+              ),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                tx.title,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                DateFormat.yMMMd().format(tx.date),
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
