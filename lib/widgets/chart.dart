1de7f46d9e583e79cc2f5331e08c4135e85592de
Saharat Paynok <saharat.paynok@gmail.com>
2019-09-11 22:32:28 +0700

    [Lec 96]: Looping Through Lists

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:personal_expenses_tracker/models/transaction.dart';
+import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      double totalSum = 0.0;
+
+      recentTransactions.forEach((element) {
+        if (element.date.day == weekDay.day &&
+            element.date.month == weekDay.month &&
+            element.date.year == weekDay.year) {
+          totalSum += element.amount;
+        }
+      });
+
+      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: ,
+      child: Row(
+        children: <Widget>[],
+      ),
     );
   }
 }
